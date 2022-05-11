1e949a0bbd68bada2e2e45fbf0fef7b781356ef6
pierre souza <65835017+pierresouza@users.noreply.github.com>
2022-05-11 15:05:40 -0300

    remove old function

--- todolist/src/components/DivData/index.tsx
+++ todolist/src/components/DivData/index.tsx
@@ -7,24 +7,10 @@ import { IoIosAddCircle } from "react-icons/io";
 export const DivData: React.FC = () => {
   const [todos, setTodos] = useState<string[]>([]);
   const [value, setValue] = useState<string>("");
-
-  // const handleRemoveItem = React.useCallback(
-  //   (todo: string) => {
-  //     let newTodos = [...todos];
-  //     newTodos.splice(todos.indexOf(todo), 1);
-  //     setTodos(newTodos);
-  //   },
-  //   [todos]
-  // );
   const remover = (index:string) => {
     const deleteItem = todos.filter((item) => item !== index)
     setTodos(deleteItem)
   }
-
-
-
-
-
   const handleCheckboxChange = () => {
     console.log("pegou");
   };
@@ -38,9 +24,8 @@ export const DivData: React.FC = () => {
               <>
                 <li key={index}>
                   <div className="left">
-                    <input type={"checkbox"} className="checkbtn" onChange={() => handleCheckboxChange()}/>
-                    {todo}
-                  </div>
+                    <input type={"checkbox"} className="checkbtn" onChange={() => handleCheckboxChange()}/> {todo} 
+                    </div>
                   <IoIosRemoveCircleOutline size={24}className="deletebtn" onClick={() => remover(todo)}/>
                 </li>
               </>
