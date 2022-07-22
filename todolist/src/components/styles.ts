56e34d958e7c10704df0f1b186a668bad0f0eb67
Pierre Souza <pas@lionx.com.br>
2022-07-22 10:01:57 -0300

    STYLE: remove unused archive

--- todolist/src/components/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  box-shadow: 0px 3px 6px 1px rgba(0, 0, 0, 0.15);
-  border-radius: 5px;
-  font-family: 'Roboto', sans-serif;
-`;
